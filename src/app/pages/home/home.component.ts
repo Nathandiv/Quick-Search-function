243ad8d52f01043113249d77b88de3b3f33bb237
Nathandiv <k.n.hlungwani@gmail.com>
2024-11-12 14:55:19 +0200

    added and array

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,8 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   searchText:any;
+  heroes = [
+    {id:11,name:'Mr Hlungwani',country:'SA'},
+    { id: 12, name: 'Ms Mokoena', country: 'SA' },
+  { id: 13, name: 'Mr Smith', country: 'USA' },
+  { id: 14, name: 'Ms Patel', country: 'IN' },
+  { id: 15, name: 'Dr Chen', country: 'CN' },
+  { id: 16, name: 'Mr Okafor', country: 'NG' },
+  { id: 17, name: 'Ms Nakamura', country: 'JP' },
+  { id: 18, name: 'Dr Lopez', country: 'MX' },
+  { id: 19, name: 'Mr Singh', country: 'IN' },
+  { id: 20, name: 'Ms Carter', country: 'CA' }
+  ]
 
-  
 
   ngOnInit(): void {
     
